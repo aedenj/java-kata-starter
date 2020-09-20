ed668ec90e6dd196178529e021fa23e67c1f7e46
Aeden Jameson <aeden.jameson@gmail.com>
2020-09-20 12:07:33 -0700

    start off with parameterized test since it's useful more often than not

--- src/test/java/com/katas/SolutionTest.java
+++ src/test/java/com/katas/SolutionTest.java
@@ -3,11 +3,27 @@ package com.katas;
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+
+@DisplayName("solution tests")
 public class SolutionTest {
 
-  @Test
+  @ParameterizedTest(name = "{index} - description ....")
+  @MethodSource("scenarios")
   public void test() {
     assertThat(Solution.solution()).isEqualTo(true);
   }
 
+
+  static Stream<Arguments> scenarios() {
+      return Stream.of(
+        Arguments.of()
+      );
+  }
 }
