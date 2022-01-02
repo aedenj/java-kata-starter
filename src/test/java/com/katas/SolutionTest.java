75f0fb0642b933a5cd8b6c6328ac34442bac2e5e
Aeden Jameson <aeden.jameson@gmail.com>
2022-01-02 09:39:24 -0800

    add junit Named function

--- src/test/java/com/katas/SolutionTest.java
+++ src/test/java/com/katas/SolutionTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.Arguments;
